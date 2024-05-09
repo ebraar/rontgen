affc781cc638b545c9276f271a310b9787df17fb
ebraar <ebbet468@gmail.com>
2024-05-09 10:34:42 +0300

    9 mayıs toplantı

--- rontgen/rontgen.py
+++ rontgen/rontgen.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 
-image_path = "edges"
-img = cv2.imread("146.jpg", 0)
+img_path = 'rontgen/146.jpg'
+img = cv2.imread(img_path, 0)
 cv2.imshow("renkli resim",img)
 
 # histogram işlemi
@@ -18,15 +19,22 @@ cv2.imshow('gray scale image', gray_image)
 kernel = np.ones((5,5), np.uint8)
 
 #dilation işlemi
-img_dilation = cv2.dilate(img, kernel, iterations=1) 
+img_dilation = cv2.dilate(gray_image, kernel, iterations=1) 
 cv2.imshow('dilation image', img_dilation)
 
 # erozyon işlemi
 eroded_image = cv2.erode(img_dilation, kernel, iterations=1)
 cv2.imshow('eroded image', eroded_image)
 
+# Closing işlemi
+closing = cv2.morphologyEx(eroded_image, cv2.MORPH_CLOSE, kernel)
+cv2.imshow('closing', closing)
+
+# Opening işlemi
+# opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
 # kenar bulma işlemi
-edges = cv2.Canny(eroded_image, 100, 200)
+edges = cv2.Canny(closing, 100, 200)
 cv2.imshow("canny image", edges)
 
 cv2.waitKey(0)
